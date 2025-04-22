6020e9aa1c5e3789446fc98055de9ff566dc727d
paraspatadiya <paraspatadiya6@gmail.com>
2025-04-23 00:01:27 +0530

    Add files via upload

---
+++ Accesment/module 3/pop.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int l,b;
+	
+	printf("enter value of l:");
+	scanf("%d",&l);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	
+	printf("\narea of ractangle is:%d",l*b);
+}
