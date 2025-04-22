343c71cf7bf63d0476eea470e09cedb0b004659f
paraspatadiya <paraspatadiya6@gmail.com>
2025-04-23 00:04:04 +0530

    Add files via upload

---
+++ Assignment/module 3/development environment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n1,n2;
+	cout<<"enter number 1:";
+	cin>>n1;
+	cout<<"enter number 2:";
+	cin>>n2;
+	
+	cout<<"sum is:"<<n1+n2;
+}
---
+++ Assignment/module 3/multiplication table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n1,table;
+	cout<<"enter any number:";
+	cin>>table;
+	for(n1=1;n1<=10;n1++)
+	{
+		cout<<table<<"*"<<n1<<"="<<table*n1<<"\n";
+	}
+
+}
